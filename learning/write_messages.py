56f5352d11b37fd7f3e970c59b23a443b6c63098
Johan van der Kooij <11424878+datadutch@users.noreply.github.com>
2024-02-01 21:03:12 +0100

    firther

--- learning/write_messages.py
+++ learning/write_messages.py
@@ -13,6 +13,8 @@ students = [
     {"name": "Sandrine",  "score": 100},
     {"name": "Gergeley", "score": 87},
     {"name": "Frieda", "score": 92},
+    {"name": "Fritz", "score": 40},
+    {"name": "Sirius", "score": 75},
 ]
 
 environment = Environment(loader=FileSystemLoader("templates/"))
